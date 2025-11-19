ff05309024dc0da1bcf7f91d7341184714212e28
Anand6262 <pyexpert62@gmail.com>
2025-11-19 17:13:01 +0530

    changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,7 +18,7 @@ router.post("/signup", (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password)
-    return res.status(400).json({ message: "Username & password required" });
+    return res.status(400).json({ message: "Username & password required!" });
 
   const hashedPassword = bcrypt.hashSync(password, 10);
 
